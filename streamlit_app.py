09438855b65f718b40c9d82324a4ad8ec368da1f
Gynna Paola Mogollon Zabala <86632783+Gynnapaolamz@users.noreply.github.com>
2026-05-08 22:20:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if  ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
+#new section to display smoothiefroot nutrion information 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
